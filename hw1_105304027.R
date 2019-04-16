55a4b7e757a70cf0a2e29e5ff7b6dbb908ee752a
kuo870202 <105304027@nccu.edu.tw>
2019-04-16 11:07:32 +0800

    Revise 2

--- hw1_105304027.R
+++ hw1_105304027.R
@@ -7,20 +7,24 @@ if (sum(c('--input','--output') %in% args) != 2) {
   stop("USAGE: Rscript hw1 input", call.=FALSE)
 } else {
   if(args[1]=='--input'){
+    df <- data.frame(set=character(1),
+                    weight = double(1),
+                    height = double(1),
+                    stringsAsFactors = F)
     data1 <- read.csv(args[2])
-    data1 <- data1[,1:3]
-    names(data1)[1] <- 'set'
-    data1[[1]] <- c(gsub('.csv','',strsplit(args[2],'/')[[1]][length(strsplit(args[2],'/')[[1]])]))
-    data1[[2]] <- round(max(data1[[2]]),digits = 2)
-    data1[[3]] <- round(max(data1[[3]]),digits = 2)
-    write.csv(data1[1,],args[4])
+    df$set <- c(gsub('.csv','',strsplit(args[2],'/')[[1]][length(strsplit(args[2],'/')[[1]])]))
+    df$weight <- round(max(data1$weight),digits = 2)
+    df$height <- round(max(data1$height),digits = 2)
+    write.csv(df,args[4],row.names = F,quote = F)
   }else{
+    df <- data.frame(set=character(1),
+                     weight = double(1),
+                     height = double(1),
+                     stringsAsFactors = F)
     data1 <- read.csv(args[4])
-    data1 <- data1[,1:3]
-    names(data1)[1] <- 'set'
-    data1[[1]] <- c(gsub('.csv','',strsplit(args[4],'/')[[1]][length(strsplit(args[4],'/')[[1]])]))
-    data1[[2]] <- round(max(data1[[2]]),digits = 2)
-    data1[[3]] <- round(max(data1[[3]]),digits = 2)
-    write.csv(data1[1,],args[2])
+    df$set <- c(gsub('.csv','',strsplit(args[4],'/')[[1]][length(strsplit(args[4],'/')[[1]])]))
+    df$weight <- round(max(data1$weight),digits = 2)
+    df$height <- round(max(data1$height),digits = 2)
+    write.csv(df,args[2],row.names = F,quote = F)
   }
 }
